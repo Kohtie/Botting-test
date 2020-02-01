be7b7dcc15bc8089cf2500fc308581d047285f46
Kohtie <tuman4ik1996@gmail.com>
2020-02-01 20:28:29 +0700

    Updates

--- TestBot.py
+++ TestBot.py
@@ -3,8 +3,7 @@ telebot.apihelper.proxy = {'https':'https://138.197.5.192:8888'}
 
 bot = telebot.TeleBot('1036419164:AAHV0bQkmvJct5YLXHB7LM7sTA6VVK6GmIE')
 
-@bot.message_handler(content_types=['start'])
-
+@bot.message_handler(content_types=['text'])
 def start_message(message):
     bot.send_message(message.from_user.id, "HELLO BEAUTIFUL")
 
